443d5049fc9d6c5ff49bd1da013244ba8058960f
MBEDOBE <danielbushbaby@gmail.com>
2020-08-24 03:15:43 +0000

    changes to homescreen and productscreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from "react-router-dom";
-import "./App.css";
-import logo from './logo.svg';
 import './App.css';
 import data from './data';
 import HomeScreen from "./screens/HomeScreen";
-
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   const openMenu = () => {
@@ -24,7 +22,8 @@ function App() {
           <button onClick={openMenu}>
             &#9776;
                 </button>
-          <a href="index.html">aShop</a>
+                <Link to="/">aShop</Link>
+          
         </div>
         <div className="header-links">
           <a href="cart.html">Cart</a>
@@ -47,28 +46,9 @@ function App() {
       </aside>
       <main className="main">
         <div className="content">
-            <Route path="/products/:id" component={ProductScreen}></Route>
+            <Route path="/product/:id" component={ProductScreen}></Route>
             <Route path="/" exact={true} component={HomeScreen}></Route>
-          <ul className="products">
-            {
-              data.products.map(product =>
-                <li>
-                  <div className="product">
-                    <img className="product-image" src={product.image} alt="product" />
-                    <div className="product-name">
-                      <a href="product.html">{product.name}</a>
-                    </div>
-                    <div className="product-brand">{product.brand}</div>
-                    <div className="product-price">GHC{product.price}</div>
-                    <div className="product-rating">{product.rating} Stars ({product.numReviews})</div>
-                  </div>
-                </li>
-                )
-            }
-           
-            
-
-          </ul>
+          
         </div>
 
       </main>
