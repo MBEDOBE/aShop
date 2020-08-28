393a07c0ec98a66648f52be41624ca86d222b618
MBEDOBE <danielbushbaby@gmail.com>
2020-08-28 10:11:01 +0000

    updated store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -8,5 +8,5 @@ const reducer = combineReducers({
     productList: productListReducer,
 })
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, initialState, compose(applyMiddleware(thunk)));
+const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
 export default store;
